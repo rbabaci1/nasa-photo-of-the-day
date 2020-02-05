32d7f1de0a2beec9a3103947b573e1a9e21d5d65
rabah babaci <4rabah@gmail.com>
2020-02-04 16:04:01 -0800

    import Intro component to App

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import Intro from "./Introduction/intro";
 
 import "./App.css";
 
 function App() {
-  axios
-    .get(
-      "https://api.nasa.gov/insight_weather/?api_key=DEMO_KEY&feedtype=json&ver=1.0"
-    )
-    .then(response => console.log(response.data))
-    .catch(error => console.error(error));
+  // axios
+  //   .get(
+  //     "https://api.nasa.gov/insight_weather/?api_key=DEMO_KEY&feedtype=json&ver=1.0"
+  //   )
+  //   .then(response => console.log(response.data))
+  //   .catch(error => console.error(error));
 
   return (
     <div className="App">
-      <div className="container"></div>
+      <Intro />
     </div>
   );
 }
---
+++ src/SolCardCreation/solCardCreation.js
@@ -0,0 +1,7 @@
+import react from "react";
+
+function SolCardCreation() {
+  return null;
+}
+
+export default SolCardCreation;
