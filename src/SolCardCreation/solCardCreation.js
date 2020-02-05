0f6f05daa88e8b23e607863c22c6939135d38798
rabah babaci <4rabah@gmail.com>
2020-02-04 17:23:20 -0800

    send a get request and set the weather data to the returned response

--- src/SolCardCreation/solCardCreation.js
+++ src/SolCardCreation/solCardCreation.js
@@ -6,6 +6,15 @@ import axios from "axios";
 function SolCardCreation() {
   const [weatherData, setWeatherData] = useState([]);
 
+  function effectFunction() {
+    axios
+      .get(
+        "https://api.nasa.gov/insight_weather/?api_key=DEMO_KEY&feedtype=json&ver=1.0"
+      )
+      .then(response => setWeatherData(response.data))
+      .catch(error => console.error(error));
+  }
+
   useEffect(() => {}, []);
 
   return null;
