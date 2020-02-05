482b7826695aaa56c5c300f171132aa5100a2016
rabah babaci <4rabah@gmail.com>
2020-02-04 16:18:08 -0800

    add an image folder

--- src/Introduction/intro.js
+++ src/Introduction/intro.js
@@ -1,7 +1,12 @@
-import react from "react";
+import React from "react";
 
 function Intro() {
-  return null;
+  return (
+    <div className="intro">
+      <h1>Latest Weather</h1>
+      <h1>at Elysium Planitia 🪐</h1>
+    </div>
+  );
 }
 
 export default Intro;
